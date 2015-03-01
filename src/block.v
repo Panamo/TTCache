49925ce41d5a866611dd22a082c1a053939da5c7
Parham Alvani <parham.alvani@gmail.com>
2015-03-01 21:22:16 +0330

    block module improved.

--- src/block.v
+++ src/block.v
@@ -1,17 +1,9 @@
-module block(enable, comp, write, tag_in, data_in,
-	valid_in, hit, dirty, tag_out, data_out, valid);
+module block(enable, comp, write, data_in, data_out);
 	
 	input enable;
-	input comp;
 	input write;
-	input [0:4] tag_in;
 	input [0:15] data_in;
-	input valid_in;
 
-	output hit;
-	output dirty;
-	output [0:4] tag_out;
 	output [0:15] data_out;
-	output valid;
 
 endmodule
