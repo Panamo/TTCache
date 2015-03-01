56ded1a979419c921c456a5d553a5ab772cf5a76
Parham Alvani <parham.alvani@gmail.com>
2015-03-01 22:57:44 +0330

    block module improved.

--- src/block.v
+++ src/block.v
@@ -1,10 +1,18 @@
-module block(enable, comp, write, data_in, data_out);
+module block(enable, write, data_in, data_out);
 	
 	input enable;
 	input write;
 	input [0:15] data_in;
 
-	output [0:15] data_out;
+	output reg [0:15] data_out;
 
 	reg [0:15] data;
+
+	always @ (enable, write) begin
+		if (enable)
+			if (write)
+				data = data_in;
+			else
+				data_out = data;
+	end
 endmodule
