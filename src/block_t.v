ab3cd320fe2cade80170ada4faf88ab6a729fda6
Parham Alvani <parham.alvani@gmial.com>
2015-03-02 03:52:35 +0330

    block module improved.

--- src/block_t.v
+++ src/block_t.v
@@ -13,10 +13,10 @@ module block_t;
 		enable = 0;
 		write = 0;
 		#5 enable = 1;
-		#5 data_in = 16'b0000_1111_0000_1111;
-		#5 write = 1;
-		#6 write = 0;
-		#100 $stop;
+		data_in = 16'b0000_1111_0000_1111;
+		write = 1;
+		#1 write = 0;
+		#10 $stop;
 	end
 	block blk(enable, write, data_in, data_out);
 endmodule
