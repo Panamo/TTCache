650048da2dae414e1d9c753694693d0e532e58fb
Parham Alvani <parham.alvani@gmial.com>
2015-03-04 21:30:39 +0330

    cache module improved.

--- src/cache.v
+++ src/cache.v
@@ -5,15 +5,17 @@
  *
  * [] Creation Date : 04-03-2015
  *
- * [] Last Modified : Wed 04 Mar 2015 08:00:44 AM IRST
+ * [] Last Modified : Wed 04 Mar 2015 09:30:24 PM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
 */
 module cache (enable, index, word, comp,
 	write, tag_in, data_in, valid_in,
-	clk, rst, hit, dirty, tag_out,
+	rst, hit, dirty, tag_out,
 	data_out, valid);
+
+	parameter N = 10;
 
 	input enable;
 	input [0:7] index;
@@ -23,33 +25,62 @@ module cache (enable, index, word, comp,
 	input [0:4] tag_in;
 	input [0:15] data_in;
 	input valid_in;
-	input clk;
 	input rst;
 
-	output hit;
-	output dirty;
-	output [0:4] tag_out;
-	output [0:15] data_out;
-	output valid;
-	
+	output reg hit;
+	output reg dirty;
+	output reg [0:4] tag_out;
+	output reg [0:15] data_out;
+	output reg valid;
+
 	/*
-	 *                 +-------------------------------------------+
-	 *        enable>--|                     set #0                |
-	 *    index[7:0]>--|-------------------------------------------|
-	 *     word[1:0]>--|                     set #1                |
-	 *          comp>--|-------------------------------------------|
-	 *         write>--| block #1 | block #2 | block #3 | block #4 |
-	 *   tag_in[0:4]>--|-------------------------------------------|
-	 * data_in[15:0]>--|
-	 *      valid_in>--|
-	 *                 |
-	 *           clk>--|
-	 *           rst>--|
-	 *                 |
-	 *                 |
-	 *                 |
-	 *                 +------------------------+
+	 *                      +-------------------+
+         *                      |                   |
+         *        enable >------|                   |
+         *    index[7:0] >------|    cache          |
+         *     word[1:0] >------|                   |
+         *          comp >------|    256 lines      |-----> hit
+         *         write >------|    by 4 words     |-----> dirty
+         *   tag_in[4:0] >------|                   |-----> tag_out[4:0]
+         * data_in[15:0] >------|                   |-----> data_out[15:0]
+         *      valid_in >------|                   |-----> valid
+         *                      |                   |
+         *           clk >------|                   |
+         *           rst >------|                   |
+         *    createdump >------|                   |
+         *                      +-------------------+
 	*/
 	
+		
+	reg set_en [0:N];
+	reg set_rst [0:N];
+	reg [0:1] set_word [0:N];
+	reg set_cmp [0:N];
+	reg set_wr [0:N];
+	reg [0:4] set_tag_in [0:N];
+	reg [0:15] set_in [0:N];
+	reg set_valid_in [0:N];
 
+	wire set_hit [0:N];
+	wire set_dirty_out [0:N];
+	wire [0:4] set_tag_out [0:N];
+	wire [0:15] set_out [0:N];
+	wire set_valid_out [0:N];
+
+	generate
+	genvar i;
+	for (i = 0; i < N; i = i + 1) begin
+		set set_ins(set_en[i], set_word[i], set_cmp[i], set_wr[i], set_tag_in[i],
+			set_in[i], set_valid_in[i], rst, set_hit[i], set_dirty_out[i],
+			set_tag_out[i], set_out[i], set_valid_out[i]);
+	end
+	endgenerate
+	
+	always @ (enable) begin
+		if (enable) begin
+			
+		end else begin
+		
+		end
+	end
 endmodule
---
+++ src/cache_ctl.v
@@ -0,0 +1,12 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : cache_ctl.v
+ *
+ * [] Creation Date : 04-03-2015
+ *
+ * [] Last Modified : Wed 04 Mar 2015 08:54:52 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
