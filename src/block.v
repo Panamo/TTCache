3939bf337a2eb253f8fa7f2cbd0e85ef76e68047
Parham Alvani <parham.alvani@gmail.com>
2015-03-30 02:34:38 +0430

    ack signal add into block.

--- src/block.v
+++ src/block.v
@@ -5,26 +5,29 @@
  *
  * [] Creation Date : 04-03-2015
  *
- * [] Last Modified : Wed 04 Mar 2015 10:45:22 AM IRST
+ * [] Last Modified : Mon 30 Mar 2015 02:31:32 AM IRDT
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
 */
-module block(enable, write, data_in, data_out);
+module block(enable, write, data_in, data_out, ack);
 	
 	input enable;
 	input write;
 	input [0:15] data_in;
 
 	output reg [0:15] data_out;
+	output reg ack;
 
 	reg [0:15] data;
 
 	always @ (enable, write) begin
+		ack = 1'b0;
 		if (enable) begin
 			if (write)
 				data = data_in;
 			data_out = data;
+			ack = 1'b1;
 		end
 	end
 endmodule
